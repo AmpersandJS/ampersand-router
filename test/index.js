461da5470bcad374bb225dc02be42943a72a387c
Philip Roberts <phil@andyet.net>
2014-06-16 12:13:32 +0100

    Switch up test-running.

--- test/index.js
+++ test/index.js
@@ -9,8 +9,8 @@ tape.Test.prototype.strictEqual = function () {
 };
 
 var Backbone = {
-    Router: require('./ampersand-router'),
-    history: require('./ampersand-history')
+    Router: require('../ampersand-router.i'),
+    history: require('../ampersand-history.i')
 };
 Backbone.History = Backbone.history.constructor;
 
